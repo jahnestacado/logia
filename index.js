d13a2d5298a6c6e9225d25138c63bee2aa8fb640
Jahn <ioannis.kiwe@gmail.com>
2017-03-27 22:45:05 +0200

    Removed stupid 'logia-' prefix from filenames

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-module.exports = require("./lib/logia-factory.js");
+/*
+* logia <https://github.com/jahnestacado/logia>
+* Copyright (c) 2016 Ioannis Tzanellis
+* Licensed under the MIT License (MIT).
+*/
+module.exports = require("./lib/factory.js");
